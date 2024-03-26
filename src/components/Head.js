51f28233fac3d71b1ea1945a28df68348b497ba1
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-03-26 11:42:32 +0530

    Created humburger toggle mrnu using redux toolkit

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,17 +1,27 @@
+import { useDispatch } from "react-redux";
+import { toogleMenu } from "../utils/appSlice";
+
 const Head = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toogleMenu());
+  };
   return (
     <div className="grid grid-flow-col p-2 m-2 shadow-lg">
       <div className="flex col-span-1 ">
         <img
-          className="h-8"
+          onClick={toggleMenuHandler}
+          className="h-8 cursor-pointer"
           src="https://imgs.search.brave.com/FM06LUSQwkDjLI2vg0vA4FenQKAApNxJVAUi52mVdzo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudGhlbm91bnBy/b2plY3QuY29tL3Bu/Zy8zMTQ0MzkzLTIw/MC5wbmc"
           alt="hamburger-menu"
         />
+        <a href="/">
         <img
           className="h-11 mx-3 "
           src="https://t3.ftcdn.net/jpg/03/00/38/90/360_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg"
           alt="youtube-logo"
-        />
+          />
+          </a>
       </div>
       <div className="col-span-10 px-10">
         <input
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,37 @@
-
+import { useSelector } from "react-redux";
+import store from "../utils/store";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  //Early return
+  if (!isMenuOpen) return null;
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="p-5 shadow-lg w-48">
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Videos</li>
+        <li>Live</li>
+        <li></li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscription</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+        <li></li>
+      </ul>
+      <h2 className="font-bold pt-5">Watch Later</h2>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
